9ab28f9c4b5f52727431c84d2c6f87e139e380da
AlexNear77 <alexgomeznear@gmail.com>
2023-05-07 23:32:55 -0500

    Functional prototype of the nomad's map

--- lib/models/sellers.dart
+++ lib/models/sellers.dart
@@ -5,31 +5,75 @@ class Sellers {
       {required this.image,
       required this.title,
       required this.address,
-      required this.location});
+      required this.location,
+      required this.enable,
+      required this.limit});
 
   final String image;
   final String title;
   final String address;
   final LatLng location;
+  final String enable;
+  final String limit;
 }
 
 final _locations = [
   LatLng(-12.0080041, -77.0778237),
   LatLng(-12.0430962, -77.0208307),
   LatLng(-12.0211287, -77.0502137),
+  LatLng(-12.0480045, -77.0205112),
+  LatLng(-12.0654067, -77.0257675),
+  LatLng(-12.0622444, -77.0708716)
 ];
 
 const _path = 'assets/images/';
 
 final mapSellers = [
-  Sellers(
+  /*  Sellers(
       image: '${_path}seller.png',
-      title: "Marcos",
-      address: 'Adress Marcos 123',
-      location: _locations[1]),
+      title: "Mario Belisario",
+      address: 'Belisario 32425',
+      location: _locations[0]), */
   Sellers(
-      image: '${_path}seller.png',
-      title: "Luigui",
-      address: "Address Luigui 321",
-      location: _locations[2])
+      image: "https://rickandmortyapi.com/api/character/avatar/6.jpeg",
+      title: "Abadango Cluster Princess",
+      address: 'Belisario 32425',
+      location: _locations[0],
+      enable: '200.12',
+      limit: '10 - 50'),
+  Sellers(
+      image: "https://rickandmortyapi.com/api/character/avatar/5.jpeg",
+      title: "Jerry Smith",
+      address: "Monrovia 2341",
+      location: _locations[1],
+      enable: '50.50',
+      limit: '5 - 30'),
+  Sellers(
+      image: "https://rickandmortyapi.com/api/character/avatar/4.jpeg",
+      title: "Beth Smith",
+      address: "Los angeles 4222",
+      location: _locations[2],
+      enable: '100.00',
+      limit: '10 - 100'),
+  Sellers(
+      image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+      title: "Rick Sanchez",
+      address: "San Andres 3249",
+      location: _locations[3],
+      enable: '70.12',
+      limit: '10 - 50'),
+  Sellers(
+      image: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+      title: "Morty Smith",
+      address: "Rovia 3221",
+      location: _locations[4],
+      enable: '63.32',
+      limit: '5 - 70'),
+  Sellers(
+      image: "https://rickandmortyapi.com/api/character/avatar/3.jpeg",
+      title: "Summer Smith",
+      address: "Matia 2341",
+      location: _locations[5],
+      enable: '70',
+      limit: '5 - 20')
 ];
--- lib/pages/map_screen.dart
+++ lib/pages/map_screen.dart
@@ -7,6 +7,8 @@ const MAPBOX_ACCES_TOKEN =
     'pk.eyJ1IjoiYWxleG5lYXIiLCJhIjoiY2xoZHhxdmxkMWZkOTNsbm9ydXRwdnNnaSJ9.1fvRUpDAO76YHw9sM-21Iw';
 const MAPBOX_STYLE = 'mapbox/dark-v11';
 const MARKER_COLOR = Color.fromARGB(255, 235, 218, 33);
+const MARKER_SIZE_EXPANDED = 67.0;
+const MARKER_SIZE_SHRINKED = 50.0;
 final myPosition = LatLng(-12.0362176, -77.0296812);
 
 class MapScreen extends StatefulWidget {
@@ -18,6 +20,7 @@ class MapScreen extends StatefulWidget {
 
 class _MapScreenState extends State<MapScreen> {
   final _pageController = PageController();
+  int _selectedIndex = 0;
 
   List<Marker> _buildMarkers() {
     final _markerList = <Marker>[];
@@ -25,16 +28,24 @@ class _MapScreenState extends State<MapScreen> {
       final mapItem = mapSellers[i];
       _markerList.add(
         Marker(
-          height: 40,
-          width: 40,
+          height: MARKER_SIZE_EXPANDED,
+          width: MARKER_SIZE_EXPANDED,
           point: mapItem.location,
           builder: (_) {
             return GestureDetector(
               //pageController.jumpTo (indice)
               onTap: () {
+                _selectedIndex = i;
+                setState(() {
+                  _pageController.animateToPage(i,
+                      duration: const Duration(milliseconds: 1000),
+                      curve: Curves.fastOutSlowIn); /* elasticOut */
+                });
                 print('Selected marker ${mapItem.title}');
               },
-              child: Image.asset('assets/images/seller.png'),
+              child: _LocationMarker(
+                selected: _selectedIndex == i,
+              ),
             );
           },
         ),
@@ -88,6 +99,8 @@ class _MapScreenState extends State<MapScreen> {
             bottom: 21,
             height: MediaQuery.of(context).size.height * 0.3,
             child: PageView.builder(
+                controller: _pageController,
+                physics: const NeverScrollableScrollPhysics(),
                 itemCount: mapSellers.length,
                 itemBuilder: (context, index) {
                   final item = mapSellers[index];
@@ -99,6 +112,27 @@ class _MapScreenState extends State<MapScreen> {
     );
   }
 }
+
+//=============================================================================
+//                    Animation of the mark when selecting it
+//============================================================================
+class _LocationMarker extends StatelessWidget {
+  const _LocationMarker({super.key, this.selected = false});
+  final bool selected;
+  @override
+  Widget build(BuildContext context) {
+    final size = selected ? MARKER_SIZE_EXPANDED : MARKER_SIZE_SHRINKED;
+    return Center(
+      child: AnimatedContainer(
+        height: size,
+        width: size,
+        duration: Duration(milliseconds: 200),
+        child: Image.asset('assets/images/seller.png'),
+      ),
+    );
+  }
+}
+//===========================================================================
 
 class _MyLocationMarker extends StatelessWidget {
   const _MyLocationMarker({super.key});
@@ -114,6 +148,9 @@ class _MyLocationMarker extends StatelessWidget {
   }
 }
 
+//==============================================================================
+//                            Card Seller select
+//=============================================================================
 class _MapItemDetails extends StatelessWidget {
   const _MapItemDetails({super.key, required this.mapMarker});
 
@@ -124,7 +161,7 @@ class _MapItemDetails extends StatelessWidget {
         color: Colors.black, fontSize: 18, fontWeight: FontWeight.bold);
     final _styleAddress = TextStyle(color: Colors.grey[800], fontSize: 14);
     return Padding(
-      padding: const EdgeInsets.all(15.0),
+      padding: const EdgeInsets.all(14.0),
       child: Card(
         margin: EdgeInsets.zero,
         color: Colors.amber[400],
@@ -134,9 +171,21 @@ class _MapItemDetails extends StatelessWidget {
             Expanded(
               child: Row(
                 children: [
-                  Expanded(child: Image.asset(mapMarker.image)),
+                  Container(
+                    padding: EdgeInsets.only(left: 10, right: 10),
+                    child: SizedBox(
+                        height: 120,
+                        width: 120,
+                        child: ClipRRect(
+                            borderRadius: BorderRadius.circular(100),
+                            child: Image.network(mapMarker.image))),
+                  ),
+
+                  /* Expanded(child: Image.network(mapMarker.image)), */
+                  /* Expanded(child: Image.asset(mapMarker.image)), */
                   Expanded(
                       child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     mainAxisSize: MainAxisSize.min,
                     children: [
                       Text(
@@ -144,26 +193,64 @@ class _MapItemDetails extends StatelessWidget {
                         style: _styleTitle,
                       ),
                       const SizedBox(height: 10),
-                      Text(mapMarker.address, style: _styleTitle)
+                      Text("Address: " + mapMarker.address,
+                          style: const TextStyle(
+                              color: Color.fromARGB(255, 41, 41, 41),
+                              fontSize: 17)),
+                      Row(children: [
+                        const Text("Enable: ",
+                            style: TextStyle(
+                                color: Color.fromARGB(255, 41, 41, 41),
+                                fontSize: 17)),
+                        Text("\$" + mapMarker.enable,
+                            style: const TextStyle(
+                                color: Color.fromARGB(255, 35, 231, 58),
+                                fontSize: 17)),
+                      ]),
+                      Text("Limit: \$" + mapMarker.limit,
+                          style: const TextStyle(
+                              color: Color.fromARGB(255, 52, 52, 52),
+                              fontSize: 17))
                     ],
                   ))
                 ],
               ),
             ),
-            MaterialButton(
+            Padding(
+              padding: EdgeInsets.fromLTRB(14.0, 0.0, 14.0, 7.0),
+              child: Material(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(22.0)),
+                elevation: 17.0,
+                color: Colors.black,
+                clipBehavior: Clip.antiAlias,
+                child: MaterialButton(
+                  minWidth: 200.0,
+                  height: 35,
+                  color: Colors.black,
+                  child: Text(
+                    "Trade",
+                    style: TextStyle(fontSize: 16, color: Colors.white),
+                  ),
+                  onPressed: () {},
+                ),
+              ),
+            ),
+            /* MaterialButton(
               padding: EdgeInsets.zero,
               onPressed: () => null,
-              color: MARKER_COLOR,
+              color: Colors.black,
               elevation: 6,
               child: Text(
                 'Trade',
                 style:
-                    TextStyle(fontWeight: FontWeight.bold, color: Colors.black),
+                    TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
               ),
-            )
+            ) */
           ],
         ),
       ),
     );
   }
 }
+//========================================================================
